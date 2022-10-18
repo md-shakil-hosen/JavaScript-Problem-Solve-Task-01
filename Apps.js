5fd5924314dabcb0bcd3ed0be5c2ca0c65f7fe19
md-shakil-hosen <shakilinfo91@gmail.com>
2022-10-19 00:36:50 +0600

    leap year

--- Apps.js
+++ Apps.js
@@ -95,4 +95,22 @@ function odd_even(){
 	}
 	}
 
-	// ---------------Check Whether a number is even or odd----------------
+	// ---------------Check Whether a Year is leap year or not----------------
+	function check_leapyear(){
+		//define variables
+		var year;
+		//get the entered year from text box 
+		year = document.getElementById("year").value;
+ 
+		//three conditions to find out the leap year
+		if( (0 == year % 4) && (0 != year % 100) || (0 == year % 400) )
+		{
+			alert(year+" is a leap year");  
+		}
+		else
+		{
+			alert(year+" is not a leap year");  
+		}
+	}
+	
+	
